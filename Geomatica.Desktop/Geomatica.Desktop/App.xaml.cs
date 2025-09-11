ceb3c6b6a493020ad2517fc85ff6321496c7f4b9
Marbin Javier Arévalo Guerrero <loqndro@gmail.com>
2025-09-11 01:51:43 -0500

    Mejoras en la estructura y autenticación de la app
    Se ha añadido la referencia al espacio de nombres `local` en `App.xaml` y se han incorporado nuevas directivas `using` en `App.xaml.cs`.
    
    Se implementa la lectura de la clave API desde `API_KEY.txt` para mayor flexibilidad.
    
    El `TargetFramework` se actualiza en `Geomatica.Desktop.csproj` para mejorar la compatibilidad.
    
    Se elimina `AuthenticationHelper.cs` y se simplifican las definiciones de `MapView` en `MainWindow.xaml` y `MapaView.xaml`, adoptando un enfoque MVVM con `MapViewModel`.
    
    Se introduce `ArcGISLoginPrompt.cs` para la autenticación OAuth, mejorando la gestión de credenciales.
    
    Se añaden comentarios de licencia en varios archivos, indicando que el código está bajo la Licencia Apache 2.0.

--- Geomatica.Desktop/Geomatica.Desktop/App.xaml.cs
+++ Geomatica.Desktop/Geomatica.Desktop/App.xaml.cs
@@ -1,5 +1,11 @@
-﻿using System.Windows;
-using Esri.ArcGISRuntime;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace Geomatica.Desktop
 {
@@ -7,9 +13,14 @@ namespace Geomatica.Desktop
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            // Si tienes API key de ArcGIS, descomenta:
-            // ArcGISRuntimeEnvironment.ApiKey = "TU_API_KEY";
             base.OnStartup(e);
+
+            // Leer la clave API desde el archivo API_KEY.txt
+            string apiKey = File.ReadAllText("API_KEY.txt").Trim();
+            Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.ApiKey = apiKey;
+
+            // Call a function to set up the AuthenticationManager for OAuth.
+            UserAuth.ArcGISLoginPrompt.SetChallengeHandler();
         }
     }
 }
---
+++ Geomatica.Desktop/Geomatica.Desktop/MapViewModel.cs
@@ -0,0 +1,71 @@
+﻿//   Copyright 2021 Esri
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Esri.ArcGISRuntime.Mapping;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Esri.ArcGISRuntime.Geometry;
+
+namespace DisplayAMap
+{
+
+    internal class MapViewModel : INotifyPropertyChanged
+    {
+
+        public MapViewModel()
+        {
+            SetupMap();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private Map? _map;
+        public Map? Map
+        {
+            get { return _map; }
+            set
+            {
+                _map = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void SetupMap()
+        {
+
+            // Create a new map with a 'topographic vector' basemap.
+            var map = new Map(BasemapStyle.ArcGISTopographic);
+
+            // Set the initial viewpoint around the Santa Monica Mountains in California.
+            var mapCenterPoint = new MapPoint(-118.805, 34.027, SpatialReferences.Wgs84);
+            map.InitialViewpoint = new Viewpoint(mapCenterPoint, 100000);
+
+            // Set the view model's Map property with the map.
+            Map = map;
+
+        }
+
+    }
+
+}
+
